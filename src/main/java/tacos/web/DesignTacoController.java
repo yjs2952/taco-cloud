a4241207ad6ef45e8f4ec6c177492f1e463791aa
yjs2952 <yjs2952@naver.com>
2021-01-01 00:03:23 +0900

    feature : 디자인 페이지 추가

---
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -0,0 +1,52 @@
+package tacos.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tacos.Ingredient;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import tacos.Ingredient.Type;
+import tacos.Taco;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+        );
+
+        for (Type type : Type.values()) {
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        }
+
+        model.addAttribute("taco", new Taco());
+
+        return "design";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(x -> x.getType() == type)
+                .collect(Collectors.toList());
+    }
+}
