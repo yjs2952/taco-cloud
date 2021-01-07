567516ee17088dfc9b3ee535c6d3bc9323d74742
yjs2952 <yjs2952@naver.com>
2021-01-07 19:29:04 +0900

    feature : entity 추가

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,10 +1,16 @@
 package tacos;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
 
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Entity
 public class Ingredient {
     private final String id;
     private final String name;
