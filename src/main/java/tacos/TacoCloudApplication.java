48ecc7e5d8ba0909510e72f3f80dbec27124dc9a
yjs2952 <yjs2952@naver.com>
2021-01-07 19:36:44 +0900

    feature : 초기 데이터 입력

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -1,7 +1,11 @@
 package tacos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import tacos.Ingredient.Type;
+import tacos.data.IngredientRepository;
 
 @SpringBootApplication
 public class TacoCloudApplication {
@@ -10,4 +14,19 @@ public class TacoCloudApplication {
         SpringApplication.run(TacoCloudApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner dataLoader(IngredientRepository repository) {
+        return args -> {
+            repository.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+            repository.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+            repository.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+            repository.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+            repository.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+            repository.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+            repository.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+            repository.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+            repository.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
+            repository.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+        };
+    }
 }
