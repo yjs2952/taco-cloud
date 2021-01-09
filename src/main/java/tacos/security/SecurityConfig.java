bcd4133c9011ba54c8af4949a65ed4a3e2e71c90
yjs2952 <yjs2952@naver.com>
2021-01-09 16:14:34 +0900

    feature : jdbc 기반 사용자 스토어 구성

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,14 +1,20 @@
 package tacos.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,13 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-            .inMemoryAuthentication()
-            .withUser("user1")
-            .password("{noop}password1")
-            .authorities("ROLE_USER")
-            .and()
-            .withUser("user2")
-            .password("{noop}password2")
-            .authorities("ROLE_USER");
+            .jdbcAuthentication()
+            .dataSource(dataSource);
     }
 }
