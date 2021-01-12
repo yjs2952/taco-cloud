2cb0b7a79e23cc17d5d0ea06f3beeaa1e1343d80
yjs2952 <yjs2952@naver.com>
2021-01-12 23:55:58 +0900

    feature : 프로필 지정

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import tacos.Ingredient.Type;
 import tacos.data.IngredientRepository;
 
@@ -15,6 +16,8 @@ public class TacoCloudApplication {
     }
 
     @Bean
+//    @Profile({"dev", "qa"})
+//    @Profile("!prod")
     public CommandLineRunner dataLoader(IngredientRepository repository) {
         return args -> {
             repository.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
