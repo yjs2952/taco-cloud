fa23e32d0788c27c80a95a7a8ba6e3e910076bd8
yjs2952 <yjs2952@naver.com>
2021-01-13 23:29:02 +0900

    feature : RestController 추가

---
+++ src/main/java/tacos/api/DesignTacoController.java
@@ -0,0 +1,29 @@
+package tacos.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tacos.Taco;
+import tacos.data.TacoRepository;
+
+@RestController
+@RequestMapping(path = "/design", produces = "application/json")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+public class DesignTacoController {
+    private final TacoRepository tacoRepository;
+
+//    @Autowired
+//    private EntityLinks entityLinks;
+
+    @GetMapping("/recent")
+    public Iterable<Taco> recentTacos() {
+        PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+        return tacoRepository.findAll(page).getContent();
+    }
+}
--- src/main/java/tacos/data/TacoRepository.java
+++ src/main/java/tacos/data/TacoRepository.java
@@ -1,7 +1,10 @@
 package tacos.data;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import tacos.Taco;
 
 public interface TacoRepository extends CrudRepository<Taco, Long> {
+    Page<Taco> findAll(Pageable pageable);
 }
